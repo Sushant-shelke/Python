3a5aa0a0f1ef6a71c1b59211bf79a31dc40a87ed
Sushant-shelke <sushantmohan.s2006@outlook.com>
2025-09-20 01:05:57 +0530

    Handling Exceptions from l3

---
+++ Lecture 3/distances.py
@@ -0,0 +1,31 @@
+distances = {
+    "Voyager 1": "163",
+    "Voyager 2": "136",
+    "Pioneer 10": "80 AU",
+    "New Horizons": "58",
+    "Pioneer 11": "44 AU"
+}
+
+
+def main():
+    spacecraft = input("Enter a spacecraft: ")
+
+    try:
+        au = float(distances[spacecraft])
+    except KeyError:
+        print(f"'{spacecraft}' is not in dictionary")
+        return
+
+    except ValueError:
+        print("Can't convert")
+        return
+
+    m = convert(au)
+    print(f"{m} m away")
+
+
+def convert(au):
+    return au * 149597870700
+
+
+main()
